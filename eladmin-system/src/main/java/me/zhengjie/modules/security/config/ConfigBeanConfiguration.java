85686c4f2019721fa38add03f457d4eadb4a8ba5
vincen <vincen.gao@outlook.com>
2022-06-05 23:41:36 +0800

    - 替换数据库连接池：druid —> hikari - 删除 log4jdbc.log4j2.properties。 - 删除 SQL 监控相关代码。

--- eladmin-system/src/main/java/me/zhengjie/modules/security/config/ConfigBeanConfiguration.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/security/config/ConfigBeanConfiguration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2019-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package me.zhengjie.modules.security.config;
 
 import me.zhengjie.modules.security.config.bean.LoginProperties;
@@ -22,9 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @apiNote 配置文件转换Pojo类的 统一配置 类
- * @author: liaojinlong
- * @date: 2020/6/10 19:04
+ * 配置文件转换 Pojo 类的 统一配置类
  */
 @Configuration
 public class ConfigBeanConfiguration {
